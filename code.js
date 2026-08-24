dd9c51f209e06db98ba37ec6096d0347b6712a3a
nicholas <aigberuan6@gmail.com>
2026-08-24 11:15:22 +0100

    docs: add basic JS sample code to code.js

---
+++ code.js
@@ -0,0 +1,59 @@
+// let name11 = 'mark';
+// console.log( "hi" + " " + name11);
+
+// the use of strings
+// let greet = "Good morning";
+
+// console.log(greet.length);
+
+// console.log(greet);
+
+// let firstword = "Java"
+// let secondword = "Script"
+
+// console.log(firstword + secondword);
+
+// let x = 5;
+// let y = 6;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+
+// let x = " mark is 10";
+// console.log(x);
+
+// let score = 10;
+
+// if (score >= 90) {
+//     console.log("you passed!");
+// } else {
+//     console.log("you failed!");
+// }
+
+function sayHello() {
+    console.log("hello mark");
+}
+
+
+sayHello();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
